8bdfe051849afcf9e941588793d5c646a849fc0b
David Kisluk <comphack@gmail.com>
2014-05-11 21:40:53 -0400

    Shrunk the leak a little
    Now there's two separate leaks: 4 bytes, 12 bytes.
    Losing fewer bytes! :D

--- BitmapVectorization/BitmapVectorization/ImageLoader.cpp
+++ BitmapVectorization/BitmapVectorization/ImageLoader.cpp
@@ -16,6 +16,8 @@ ImageLoader::~ImageLoader(void)
 
 RawImage* ImageLoader::loadImage( char* a_path, int a_width, int a_height )
 {
+	RawImage* pImage = new RawImage( 0, 0 );
+
 	ifstream fileIn( a_path, ios::binary );
 
 	if ( fileIn.is_open() )
@@ -30,29 +32,25 @@ RawImage* ImageLoader::loadImage( char* a_path, int a_width, int a_height )
 
 		pFileContents[ length ] = 0;
 
-		RawImage* pImage = parseRawData( pFileContents, a_width, a_height );
-		return pImage;
-
+		pImage = parseRawData( pFileContents, a_width, a_height );
 		delete[] pFileContents;
 	}
 
-	RawImage* pDefaultImage = new RawImage( 0, 0 );
-
-	return pDefaultImage;
+	return pImage;
 }
 
-RawImage* ImageLoader::parseRawData( char* data, int a_width, int a_height )
+RawImage* ImageLoader::parseRawData( char* a_pData, int a_width, int a_height )
 {
 	RawImage* pImage = new RawImage( a_width, a_height );
 	const unsigned int TOTAL_COLOR_CHANNELS = a_width * a_height * 3;
 
 	for( int index = 0; index < TOTAL_COLOR_CHANNELS; index += 3 )
 	{
-		int r = ( data[ index ] + 256 ) % 256;
-		int g = ( data[ index + 1 ] + 256 ) % 256;
-		int b = ( data[ index + 2 ] + 256 ) % 256;
-
-		cout << r << "\t" << g << "\t" << b << endl;
+		// Arithmetic voodoo
+		// @TODO: Figure out why this works
+		int r = ( a_pData[ index ] + 256 ) % 256;
+		int g = ( a_pData[ index + 1 ] + 256 ) % 256;
+		int b = ( a_pData[ index + 2 ] + 256 ) % 256;
 	}
 
 	return pImage;
